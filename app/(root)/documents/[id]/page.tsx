80f5b76b4d041fc54d51f0b43e030e3520cc793a
Eduardo <ecsdebritocioni@gmail.com>
2025-05-13 12:04:17 +0200

    Initial commit

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,15 @@
+import CollaborativeRoom from '@/components/CollaborativeRoom'
+import React from 'react'
+
+
+
+const Documents = () => {
+  return (
+    <main className='flex w-full flex-col items-center'>
+      <CollaborativeRoom 
+      />
+    </main>
+  )
+}
+
+export default Documents
